1345764233840277952d085192cc76865cad189f
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-08 01:31:21 -0300

    APICARS-02: Fix class name in test

--- src/app/shared/components/toolbar/toolbar.component.spec.ts
+++ src/app/shared/components/toolbar/toolbar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarComponent } from './toolbar.component';
 
-describe('HeaderComponent', () => {
+describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
 
