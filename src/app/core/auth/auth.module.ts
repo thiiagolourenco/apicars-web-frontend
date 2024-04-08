b670a26dcbf0e4424382cc23139dfc513780f660
Thiago Lourenço <tlcb@ecomp.poli.br>
2024-04-07 21:45:42 -0300

    APICARS-01: Create shared module

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,10 +4,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  imports: [FormsModule, CommonModule, AuthRoutingModule, ReactiveFormsModule],
-  exports: [],
+  imports: [
+    SharedModule,
+    FormsModule,
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+  ],
   declarations: [LoginComponent],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
